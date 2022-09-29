8170f69c9aa6910daa052c22c6d9887c725296f1
NguyenMinhQuang001 <quangnguyenminh2601@gmail.com>
2022-09-29 10:52:00 +0700

    first commit

---
+++ QuanLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/QuanLyChuyenBayJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyChuyenBayJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyChuyenBayJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyChuyenBayJpaApplication.class, args);
+	}
+
+}
---
+++ QuanLyChuyenBayJPA/src/test/java/com/example/QuanLyChuyenBayJPA/QuanLyChuyenBayJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyChuyenBayJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyChuyenBayJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
