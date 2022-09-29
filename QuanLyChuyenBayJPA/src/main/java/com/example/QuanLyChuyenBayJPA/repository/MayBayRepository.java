3941ff54171880b4fd03ad07a7b47b8745d5baf2
NguyenMinhQuang001 <quangnguyenminh2601@gmail.com>
2022-09-29 15:43:08 +0700

    Complete EX2

---
+++ QuanLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/repository/MayBayRepository.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyChuyenBayJPA.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.QuanLyChuyenBayJPA.entity.MayBay;
+
+public interface MayBayRepository extends JpaRepository<MayBay, String>{
+	@Query(value = "select *from maybay where TamBay > 10000", nativeQuery = true)
+	public List<MayBay> findMayBayTamBayLH10000();
+}
--- QuanLyChuyenBayJPA/src/test/java/com/example/QuanLyChuyenBayJPA/QuanLyChuyenBayJpaApplicationTests.java
+++ QuanLyChuyenBayJPA/src/test/java/com/example/QuanLyChuyenBayJPA/QuanLyChuyenBayJpaApplicationTests.java
@@ -7,21 +7,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.QuanLyChuyenBayJPA.entity.ChuyenBay;
+import com.example.QuanLyChuyenBayJPA.entity.MayBay;
 import com.example.QuanLyChuyenBayJPA.repository.ChuyenBayRepository;
+import com.example.QuanLyChuyenBayJPA.repository.MayBayRepository;
 
 @SpringBootTest
 class QuanLyChuyenBayJpaApplicationTests {
 	@Autowired
 	ChuyenBayRepository chuyenBayRepository;
+	
+	@Autowired
+	MayBayRepository mayBayRepository;
+	
+//	1.	Cho biết các chuyến bay đi Đà Lạt (DAD).
+//	@Test
+//    void cau1() {
+//        List<ChuyenBay> chuyenBays ;
+//        chuyenBays = chuyenBayRepository.findChuyenBaysDaLat();
+//        for (ChuyenBay chuyenBay : chuyenBays) {
+//            System.out.println(chuyenBay);
+//        }
+//    }
 
+//	2.	Cho biết các loại máy bay có tầm bay lớn hơn 10,000km.
 	@Test
-    void cau1() {
-        List<ChuyenBay> chuyenBays ;
-
-        chuyenBays = chuyenBayRepository.findChuyenBaysDaLat();
-        for (ChuyenBay chuyenBay : chuyenBays) {
-            System.out.println(chuyenBay);
+    void cau2() {
+        List<MayBay> mayBays ;
+        mayBays = mayBayRepository.findMayBayTamBayLH10000();
+        for (MayBay mb : mayBays) {
+            System.out.println(mb);
         }
     }
+	
+	
 
 }
