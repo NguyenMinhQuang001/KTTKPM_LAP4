617088182ef4395766fe95da4e018d2cd8132ed1
QuangNguyen <quangnguyenminh2601@gmail.com>
2022-09-30 17:27:27 +0700

    Complete SpringbootAPI+Postman EX2

--- QuanLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/controller/QuanLyChuyenBayController.java
+++ QuanLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/controller/QuanLyChuyenBayController.java
@@ -3,14 +3,13 @@ package com.example.QuanLyChuyenBayJPA.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.QuanLyChuyenBayJPA.repository.ChuyenBayRepository;
 import com.example.QuanLyChuyenBayJPA.entity.ChuyenBay;
+import com.example.QuanLyChuyenBayJPA.entity.MayBay;
+import com.example.QuanLyChuyenBayJPA.repository.ChuyenBayRepository;
+import com.example.QuanLyChuyenBayJPA.repository.MayBayRepository;
 
 
 @RestController
@@ -18,9 +17,19 @@ public class QuanLyChuyenBayController {
 	@Autowired
 	ChuyenBayRepository chuyenBayRepository;
 	
+	@Autowired
+	MayBayRepository mayBayRepository;
+	
 	@GetMapping("/didalat")
 	public List<ChuyenBay> findChuyenBaysDaLat(){
 		return chuyenBayRepository.findChuyenBaysDaLat();
 	}
+	
+	@GetMapping("/cau2")
+	public List<MayBay> findMayBayTamBayLH10000(){
+		return mayBayRepository.findMayBayTamBayLH10000();
+	}
+	
+	
 	
 }
